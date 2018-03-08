b3ab5598914a7323dc66f09b9cc5e54fe990947e
Hajk <tunczorx@gmail.com>
2018-03-08 12:06:29 +0100

    Made app which change background colour after clicking button.
    Some loops and few new methods used.

--- ColourChanger/Form1.cs
+++ ColourChanger/Form1.cs
@@ -16,5 +16,34 @@ namespace ColourChanger
         {
             InitializeComponent();
         }
+
+        //Clicking button will start background colour changer
+        //Using some loops to get smooth colour change
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            { 
+            int c = 0;
+            while  (c < 255 && Visible) //After closing program it was still working so i had to add one more condition.
+            {
+                BackColor = Color.FromArgb(c, 255 - c, c); // We could use siple colours like blue/red/green but instead we use Color.FromArgb to mix some colors
+                Application.DoEvents();                    // It pause loop for a while to refresh background colour and check if user didnt click anything etc.
+                System.Threading.Thread.Sleep(1);          // Stops loop for 1 ms -> 0,001 sec.
+                c++;
+
+               if (c == 255)
+                {
+                    while (c > 0)
+                    {
+                        BackColor = Color.FromArgb(c, 255 - c, c);
+                        Application.DoEvents();
+                        System.Threading.Thread.Sleep(1);
+                        c--;
+                    }
+                }
+                }
+            }
+        }
     }
-}
+    }
+
